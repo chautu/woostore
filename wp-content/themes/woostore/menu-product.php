4a5f74dd2dd4e56d9f04b0e03bd6224aff603681
Hang <hangnt036@gmail.com>
2022-06-16 11:18:26 +0700

    main: woo

---
+++ wp-content/themes/woostore/menu-product.php
@@ -0,0 +1,17 @@
+<nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border-top-0 border-bottom-0"
+    id="navbar-vertical">
+    <div class="navbar-nav w-100 overflow-hidden sidebar-nav" style="height: 600px;">
+        <div class="menu-category">
+            <?php wp_nav_menu( 
+                                array( 
+                                    'theme_location' => 'category-product-menu', //id tạo menu
+                                    'container' => 'false', 
+                                    'menu_id' => 'category-product-menu', 
+                                    'menu_class' => 'category-product-menu',
+                                ) 
+                            ); ?>
+        </div>
+
+    </div>
+
+</nav>
