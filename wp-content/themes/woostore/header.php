aee82de62be82cbc69dbba498729e4d5df196982
Hang <hangnt036@gmail.com>
2022-06-16 11:06:22 +0700

    main: insert theme woocommerce

--- wp-content/themes/woostore/header.php
+++ wp-content/themes/woostore/header.php
@@ -140,30 +140,13 @@
         <div class="container glo-container mb-5">
             <div class="row border-top px-xl-5">
                 <div class="col-lg-3 d-none d-lg-block sidebar-contain">
-                    <nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border-top-0 border-bottom-0"
-                        id="navbar-vertical">
-                        <div class="navbar-nav w-100 overflow-hidden sidebar-nav" style="height: 600px;">
-                            <div class="menu-category">
-                                <?php wp_nav_menu( 
-                                array( 
-                                    'theme_location' => 'category-product-menu', //id tạo menu
-                                    'container' => 'false', 
-                                    'menu_id' => 'category-product-menu', 
-                                    'menu_class' => 'category-product-menu',
-                                ) 
-                            ); ?>
-                            </div>
-
-                        </div>
-
-                    </nav>
+                    <?php get_template_part('menu-product'); ?>
                 </div>
                 <div class="col-lg-9">
                     <?php get_template_part('banner'); ?>
                 </div>
             </div>
         </div>
-
     </div>
 
     <!-- Navbar End -->
