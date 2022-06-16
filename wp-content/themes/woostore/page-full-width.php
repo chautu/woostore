dbe7239ad722cb796ae7fc8909aed1b893e7d1ae
tuct <tuctv@glotechvn.com>
2022-06-16 11:25:03 +0700

    main: add pade full width

--- wp-content/themes/woostore/page-full-width.php
+++ wp-content/themes/woostore/page-full-width.php
@@ -1,9 +1,14 @@
+<?php 
+    /* Template Name: Page full width */
+?>
+
+
 <?php get_header(); ?>
 <div id="content">
     <div class="product-box glo-page-category">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <?php if (have_posts()) : ?>
                     <?php while (have_posts()) : the_post(); ?>
                     <?php setpostview(get_the_ID()); ?>
@@ -17,9 +22,6 @@
                     </div>
                     <?php endwhile;?>
                     <?php endif; ?>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
-                    <?php get_template_part('layout/share/sidebar'); ?>
                 </div>
             </div>
         </div>
